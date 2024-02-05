535bf9a0f81012a7ed1540c826c60025119959a5
ws200 <ws200@NKANF>
2024-02-05 11:27:48 +0800

    添加项目文件。

---
+++ RimWorldCollectionDownloader/HttpHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RimWorldCollectionDownloader
+{
+    internal class HttpHelper
+    {
+        public static Task<HttpResponseMessage> PostAsync(string url, Dictionary<string, string> content)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var httpContent = new FormUrlEncodedContent(content);
+                return httpClient.PostAsync(url, httpContent);
+            }
+        }
+    }
+}
---
+++ RimWorldCollectionDownloader/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RimWorldCollectionDownloader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Publishedfiledetails _publishedfiledetails;
+        public Publishedfiledetails publishedfiledetails
+        {
+            get
+            {
+                return _publishedfiledetails;
+            }
+            set
+            {
+                _publishedfiledetails = value;
+                this.RefreshWorkshopItemsListView();
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void RefreshWorkshopItemsListView()
+        {
+            WorkshopItemsListView.ItemsSource = null;
+            WorkshopItemsListView.Items.Clear();
+            WorkshopItemsListView.ItemsSource = publishedfiledetails;
+        }
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            var testPublishedfiles = new Publishedfiledetails {
+                new Publishedfiledetail { title = "test"} ,
+            new Publishedfiledetail {title = "test2"} };
+            publishedfiledetails = testPublishedfiles;
+        }
+    }
+}
